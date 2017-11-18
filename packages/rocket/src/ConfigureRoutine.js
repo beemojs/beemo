c5e6e03b8afad570a1a15a8a4b71dabdb5f70af7
Miles Johnson <mileswjohnson@gmail.com>
2017-11-17 16:35:05 -0800

    Flatten configure routines a bit.

--- packages/rocket/src/ConfigureRoutine.js
+++ packages/rocket/src/ConfigureRoutine.js
@@ -6,13 +6,12 @@
 
 import { Routine } from 'boost';
 import Config from './configure/Config';
-import GenerateConfigsRoutine from './configure/GenerateConfigsRoutine';
-import ResolveDependenciesRoutine from './configure/ResolveDependenciesRoutine';
+import CreateConfigRoutine from './configure/CreateConfigRoutine';
+import Engine from './Engine';
 
 import type { ResultPromise } from 'boost';
 import type { ConfigureConfig } from './types';
 
-// $FlowIgnore
 export default class ConfigureRoutine extends Routine<ConfigureConfig> {
   bootstrap() {
     this.config = new Config(this.config);
@@ -23,9 +22,47 @@ export default class ConfigureRoutine extends Routine<ConfigureConfig> {
    * for every engine required for the current execution.
    */
   execute(): ResultPromise {
-    return this
-      .pipe(new ResolveDependenciesRoutine('resolve', 'Resolving dependencies'))
-      .pipe(new GenerateConfigsRoutine('generate', 'Generating configuration files', this.config))
-      .serializeSubroutines();
+    this.task('Resolving dependencies', this.resolveDependencies);
+    this.task('Generating configuration files', this.generateConfigs);
+
+    return this.serializeTasks();
+  }
+
+  /**
+   * Pipe a routine for every engine we need to create a configuration for,
+   * and then run in parallel.
+   */
+  generateConfigs(engines: Engine[]): ResultPromise {
+    engines.forEach((engine) => {
+      const routine = new CreateConfigRoutine(engine.name, engine.metadata.title);
+
+      // Make the engine easily available
+      routine.engine = engine;
+
+      this.pipe(routine);
+    });
+
+    return this.config.parallel
+      ? this.parallelizeSubroutines()
+      : this.serializeSubroutines();
+  }
+
+  /**
+   * Recursively loop through an engine's dependencies, adding a dependenct engine for each,
+   * starting from the primary engine (the command that initiated the process).
+   */
+  resolveDependencies(): Promise<Engine[]> {
+    const { primaryEngine } = this.context;
+    const queue = [primaryEngine];
+
+    while (queue.length) {
+      const engine = queue.shift();
+
+      engine.metadata.dependencies.forEach((name) => {
+        this.context.engines.unshift(this.tool.getPlugin(name));
+      });
+    }
+
+    return Promise.resolve(this.context.engines);
   }
 }
