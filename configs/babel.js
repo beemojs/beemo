020dcf0e1c04ad45535abaedb164873c9e9d3240
Miles Johnson <mileswjohnson@gmail.com>
2018-03-18 17:51:36 -0700

    Use Object.assign.

--- configs/babel.js
+++ configs/babel.js
@@ -1,8 +1,7 @@
 const baseBabel = require('@milesj/build-tool-config/configs/babel');
 
 module.exports = function babel(options) {
-  return baseBabel({
-    ...options,
+  return baseBabel(Object.assign(options, {
     node: true,
-  });
+  }));
 };
