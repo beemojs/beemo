672d0e626911c5a3728d53b67e65918642cd3e74
Miles Johnson <mileswjohnson@gmail.com>
2018-03-19 23:34:51 -0700

    Fix babel config.

--- configs/babel.js
+++ configs/babel.js
@@ -1,9 +1,9 @@
 const baseBabel = require('@milesj/build-tool-config/configs/babel');
 
 module.exports = function babel(options) {
-  return baseBabel(
-    Object.assign(options, {
-      node: true,
-    }),
-  );
+  Object.assign(options, {
+    node: true,
+  });
+
+  return baseBabel(options);
 };
