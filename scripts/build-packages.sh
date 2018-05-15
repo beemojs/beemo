2125f8fa80a4742d77c54d46fc7d9d729390c188
Miles Johnson <mileswjohnson@gmail.com>
2018-05-14 23:25:03 -0700

    Fix build script.

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -4,8 +4,18 @@ root=$PWD
 
 # We can't use Beemo to build our packages with TypeScript,
 # as Beemo's CLI requires built files... which we don't have.
-# So build them the old school way, which is faster anyways.
+# So build them the old school way, which kind of sucks.
+
+build_pkg() {
+  echo "$1"
+  cp "$root/scripts/build-tsconfig.json" "$root/$1/tsconfig.json"
+  cd "$root/$1" || exit
+  node ../../node_modules/.bin/tsc
+}
+
+build_pkg "./packages/core"
+cd "$root" || exit
+
 for pkg in ./packages/*; do
-  cd "$root/$pkg"
-  node ../../node_modules/.bin/tsc ./src/**/*.ts --outDir ./lib
+  build_pkg "$pkg"
 done
