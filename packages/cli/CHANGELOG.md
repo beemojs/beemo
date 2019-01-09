9b60e2767e3c37b7bd00e8e33a9503ad11c4aec3
Miles Johnson <mileswjohnson@gmail.com>
2019-01-08 21:03:20 -0800

    Fix debug.

--- packages/cli/CHANGELOG.md
+++ packages/cli/CHANGELOG.md
@@ -1,3 +1,13 @@
+# 0.26.0 - 12/29/2018
+
+#### 💥 Breaking
+
+- Updated `@beemo/core` peer dependency requirement to v0.26.
+
+#### 🐞 Fixed
+
+- The `--debug` flag will now enable much earlier.
+
 # 0.25.0 - 12/29/2018
 
 #### 💥 Breaking
--- packages/cli/bin/beemo.js
+++ packages/cli/bin/beemo.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
+/* eslint-disable */
 
+const debug = require('debug');
+
+// Instrument our CLI for require performance
 if (process.env.DEBUG === 'timing') {
-  // eslint-disable-next-line
   require('time-require');
+
+  // Boost doesn't enable the debugger early enough,
+  // so enable it here if we encounter the debug flag.
+} else if (process.argv.includes('--debug')) {
+  debug.enable('beemo:*');
 }
 
 require('../lib/CLI');
