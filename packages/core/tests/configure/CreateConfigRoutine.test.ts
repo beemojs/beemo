e8ca71abe820ad853e150a9206baf6e67bbf4d91
Miles Johnson <mileswjohnson@gmail.com>
2019-03-24 21:59:08 -0700

    Fix test.

--- packages/core/tests/configure/CreateConfigRoutine.test.ts
+++ packages/core/tests/configure/CreateConfigRoutine.test.ts
@@ -1,7 +1,6 @@
 import fs from 'fs-extra';
 import BabelDriver from '@beemo/driver-babel';
 import ConfigLoader from '@boost/core/lib/ConfigLoader';
-import BabelDriver from '@beemo/driver-babel';
 import CreateConfigRoutine from '../../src/configure/CreateConfigRoutine';
 import Driver from '../../src/Driver';
 import {
