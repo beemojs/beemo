e431817a3bc2bc59448c635b12b4a4e693dafadd
Miles Johnson <mileswjohnson@gmail.com>
2019-05-18 13:46:13 -0700

    Add danger js.

---
+++ dangerfile.ts
@@ -0,0 +1,21 @@
+import { danger, fail } from 'danger';
+import { checkCommitFormat } from 'conventional-changelog-beemo';
+
+// Verify the PR title contains the conventional-changelog required prefix.
+// https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-eslint
+if (!checkCommitFormat(danger.github.pr.title)) {
+  fail(
+    'Pull request title requires a conventional changelog prefix. [View commit message format](https://github.com/beemojs/conventional-changelog-beemo#commit-message-format).',
+  );
+}
+
+// When a PR only has 1 commit, and a squash merge occurs, the commit is used as-is,
+// and the PR title is lost, resulting in the semver prefix also being lost.
+if (
+  danger.github.pr.commits <= 1 &&
+  danger.github.commits[0].commit.message !== danger.github.pr.title
+) {
+  fail(
+    'Automatic releases requires commit message to match PR title if PR contains only 1 commit.',
+  );
+}
