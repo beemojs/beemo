bb2a5a958ace1ad98b26d1b4be18171110f7bf57
Miles Johnson <mileswjohnson@gmail.com>
2019-11-27 14:00:21 -0800

    Try without shell.

--- scripts/RunIntegrationTests.js
+++ scripts/RunIntegrationTests.js
@@ -41,7 +41,7 @@ module.exports = class RunIntegrationTestsScript extends Script {
     return Promise.all(
       script.split('&&').map(command =>
         execa
-          .command(command.trim(), { cwd: context.cwd.path(), preferLocal: true, shell: true })
+          .command(command.trim(), { cwd: context.cwd.path(), preferLocal: true })
           // Handles everything else
           .then(response => this.handleResult(name, options, response))
           // Handles syntax errors
