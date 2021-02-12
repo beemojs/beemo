65d7b1ac537bd3cf10c170ee5cc29fade9a44ce4
Miles Johnson <mileswjohnson@gmail.com>
2021-02-11 22:57:30 -0800

    Update config.

--- packages/local/configs/babel.js
+++ packages/local/configs/babel.js
@@ -1,12 +1,33 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 const config = {
-    babelrc: false,
-    comments: false,
-    plugins: ['@babel/plugin-proposal-class-properties'],
-    presets: [
-        ['@babel/preset-env', { targets: { node: process.version.slice(1) } }],
-        '@babel/preset-typescript',
+  babelrc: false,
+  comments: false,
+  plugins: [
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
     ],
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
+        loose: true,
+      },
+    ],
+    '@babel/plugin-proposal-export-default-from',
+    [
+      'babel-plugin-transform-dev',
+      {
+        evaluate: false,
+      },
+    ],
+    'babel-plugin-typescript-to-proptypes',
+  ],
+  presets: [
+    ['@babel/preset-env', { targets: { node: process.version.slice(1) } }],
+    '@babel/preset-typescript',
+  ],
 };
 exports.default = config;
