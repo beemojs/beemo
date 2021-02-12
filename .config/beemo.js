cd17e164c95303833676d1e80ce2e41540714748
Miles Johnson <mileswjohnson@gmail.com>
2021-02-11 17:46:14 -0800

    Add dep.

--- .config/beemo.js
+++ .config/beemo.js
@@ -1,8 +1,20 @@
 module.exports = {
   module: '@beemo/local',
-  drivers: ['babel', 'eslint', 'jest', 'mocha', 'prettier', 'typescript', 'webpack'],
+  drivers: {
+    babel: true,
+    eslint: true,
+    jest: true,
+    mocha: true,
+    prettier: true,
+    typescript: {
+      buildFolder: 'dts',
+      declarationOnly: true,
+    },
+    webpack: true,
+  },
   settings: {
     decorators: true,
     node: true,
+    react: true,
   },
 };
