f17a7ba94db19d821677420a48205e031d872c43
Miles Johnson <mileswjohnson@gmail.com>
2021-02-17 21:51:33 -0800

    types: Update config and args to inherit from `eslint`.

--- packages/driver-eslint/src/types.ts
+++ packages/driver-eslint/src/types.ts
@@ -1,97 +1,31 @@
-/* eslint-disable no-magic-numbers */
+import eslint from 'eslint';
 
-export type EnvType =
-  | 'amd'
-  | 'applescript'
-  | 'atomtest'
-  | 'browser'
-  | 'commonjs'
-  | 'embertest'
-  | 'es6'
-  | 'es2017'
-  | 'es2020'
-  | 'greasemonkey'
-  | 'jasmine'
-  | 'jest'
-  | 'jquery'
-  | 'meteor'
-  | 'mocha'
-  | 'mongo'
-  | 'nashorn'
-  | 'node'
-  | 'phantomjs'
-  | 'prototypejs'
-  | 'protractor'
-  | 'qunit'
-  | 'serviceworker'
-  | 'shared-node-browser'
-  | 'shelljs'
-  | 'webextensions'
-  | 'worker';
+export type FixType = 'layout' | 'problem' | 'suggestion';
 
-export type GlobalSetting = boolean | 'off' | 'readable' | 'readonly' | 'writable' | 'writeable';
+export type FormatType =
+  | 'checkstyle'
+  | 'codeframe'
+  | 'compact'
+  | 'html'
+  | 'jslint-xml'
+  | 'json'
+  | 'junit'
+  | 'stylish'
+  | 'table'
+  | 'tap'
+  | 'unix'
+  | 'visualstudio';
 
-export type EcmaVersion =
-  | 3
-  | 5
-  | 6
-  | 7
-  | 8
-  | 9
-  | 10
-  | 11
-  | 2015
-  | 2016
-  | 2017
-  | 2018
-  | 2019
-  | 2020
-  | 2021;
+export type GlobalSetting = boolean | 'readable' | 'readonly' | 'writable' | 'writeable';
 
-export type RuleSetting = '0' | '1' | '2' | 'error' | 'off' | 'warn' | 0 | 1 | 2;
+export type ECMAVersion = NonNullable<eslint.Linter.ParserOptions['ecmaVersion']>;
 
-export type RuleOptions = boolean | number | string | { [option: string]: unknown };
+export type Rules = eslint.Linter.RulesRecord;
 
-export interface Rules {
-  [rule: string]: RuleSetting | [RuleSetting, ...RuleOptions[]];
-}
+export type ParserOptions = eslint.Linter.ParserOptions;
 
-export interface ParserOptions {
-  [option: string]: unknown;
-  ecmaVersion?: EcmaVersion;
-  sourceType?: 'module' | 'script';
-  ecmaFeatures?: {
-    globalReturn?: boolean;
-    impliedStrict?: boolean;
-    jsx?: boolean;
-  };
-}
-
-export interface CommonConfig {
-  env?: { [env: string]: boolean } & { [K in EnvType]?: boolean };
-  extends?: string[] | string;
-  globals?: { [global: string]: GlobalSetting };
+export interface ESLintConfig extends eslint.Linter.Config {
   ignore?: string[];
-  parser?: string;
-  parserOptions?: ParserOptions;
-  plugins?: string[];
-  processor?: string;
-  rules?: Rules;
-  root?: boolean;
-  settings?: { [setting: string]: unknown };
-}
-
-export interface OverrideConfig extends CommonConfig {
-  files: string[];
-  excludedFiles?: string;
-}
-
-export interface ESLintConfig extends CommonConfig {
-  extends?: string[] | string;
-  noInlineConfig?: boolean;
-  overrides?: OverrideConfig[];
-  reportUnusedDisableDirectives?: boolean;
-  root?: boolean;
 }
 
 export interface ESLintArgs {
@@ -102,21 +36,22 @@ export interface ESLintArgs {
   color?: boolean;
   config?: string;
   debug?: boolean;
-  env?: string;
+  env?: string[];
   envInfo?: boolean;
+  errorOnUnmatchedPattern?: boolean;
   eslintrc?: boolean;
-  ext?: string;
+  ext?: string[];
   f?: string;
   fix?: boolean;
   fixDryRun?: boolean;
-  fixType?: boolean;
-  format?: string;
-  global?: string;
+  fixType?: FixType[];
+  format?: FormatType;
+  global?: string[];
   h?: boolean;
   help?: boolean;
   ignore?: boolean;
   ignorePath?: string;
-  ignorePattern?: string;
+  ignorePattern?: string[];
   init?: boolean;
   inlineConfig?: boolean;
   maxWarnings?: number;
@@ -124,13 +59,13 @@ export interface ESLintArgs {
   outputFile?: string;
   parser?: string;
   parserOptions?: object;
-  plugin?: string;
+  plugin?: string[];
   printConfig?: string;
   quiet?: boolean;
   reportUnusedDisableDirectives?: boolean;
   resolvePluginsRelativeTo?: string;
-  rule?: object;
-  rulesdir?: string;
+  rule?: object[];
+  rulesdir?: string[];
   stdin?: boolean;
   stdinFilename?: string;
   v?: boolean;
