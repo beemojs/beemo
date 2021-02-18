fbe64e5cbfe420c7db797b8e92cb281ae6322adf
Miles Johnson <mileswjohnson@gmail.com>
2021-02-17 20:31:23 -0800

    Fix code cov.

--- packages/local/src/configs/jest.ts
+++ packages/local/src/configs/jest.ts
@@ -3,7 +3,10 @@ import baseConfig from '@milesj/build-tool-config/lib/configs/jest';
 
 const config: JestConfig = {
   ...baseConfig,
-  coveragePathIgnorePatterns: ['core/src/streams'],
+  coveragePathIgnorePatterns: [
+    'core/lib', // This is being included for some reason
+    'core/src/streams',
+  ],
   testPathIgnorePatterns: ['integration'],
 };
 
