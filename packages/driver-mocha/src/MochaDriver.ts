aef5fe3adfc28e2c0fbf0050c116245aade2a00d
Miles Johnson <mileswjohnson@gmail.com>
2021-02-18 13:55:51 -0800

    types: Update config and args types.

--- packages/driver-mocha/src/MochaDriver.ts
+++ packages/driver-mocha/src/MochaDriver.ts
@@ -4,7 +4,7 @@ import { MochaConfig } from './types';
 // Success: Writes passed tests to stdout
 // Failure: Writes failed tests to stdout
 export default class MochaDriver extends Driver<MochaConfig> {
-  name = '@beemo/driver-mocha';
+  readonly name = '@beemo/driver-mocha';
 
   bootstrap() {
     this.setMetadata({
