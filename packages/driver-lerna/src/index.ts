b5573a423d33ce2e4f8906a9a7fca1bdef0225ad
Miles Johnson <mileswjohnson@gmail.com>
2021-02-18 17:11:43 -0800

    Add package.

---
+++ packages/driver-lerna/src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @copyright   2021, Miles Johnson
+ * @license     https://opensource.org/licenses/MIT
+ */
+
+import { DriverOptions } from '@beemo/core';
+import LernaDriver from './LernaDriver';
+
+export * from './types';
+
+export default function lernaDriver(options?: DriverOptions) {
+  return new LernaDriver(options);
+}
---
+++ packages/driver-lerna/src/types.ts
@@ -0,0 +1,31 @@
+export interface LernaConfig {
+  command?: {
+    bootstrap?: {
+      ignore?: string[];
+      npmClientArgs?: string[];
+      scope?: string[];
+    };
+    publish?: {
+      ignoreChanges?: string[];
+      message?: string;
+      registry?: string;
+    };
+  };
+  npmClient?: 'npm' | 'yarn';
+  packages?: string[];
+  version?: string;
+}
+
+// Only global options and does not include command options
+export interface LernaArgs {
+  concurrency?: number;
+  h?: boolean;
+  help?: boolean;
+  loglevel?: string;
+  maxBuffer?: number;
+  progress?: boolean;
+  rejectCycles?: boolean;
+  sort?: boolean;
+  v?: boolean;
+  version?: boolean;
+}
