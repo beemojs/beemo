0ae0bc2e29ed1bcaa80303d61cda469566644bbe
Miles Johnson <mileswjohnson@gmail.com>
2021-04-27 21:03:26 -0700

    Update prettier.

--- packages/local/src/configs/prettier.ts
+++ packages/local/src/configs/prettier.ts
@@ -1,9 +1,1 @@
-import { PrettierConfig } from '@beemo/driver-prettier';
-import baseConfig from '@milesj/build-tool-config/src/configs/prettier';
-
-const config: PrettierConfig = {
-  ...baseConfig,
-  ignore: baseConfig.ignore!.concat(['CHANGELOG.md']),
-};
-
-export default config;
+export { default } from '@beemo/config-prettier';
